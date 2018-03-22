7a931f40b997aec0745c4449667b85f8ba278c16
Hillary Lavin <hillarylavin@Hillarys-MacBook-Pro.local>
2018-03-22 13:38:11 -0400

    Create preliminary JS pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// Create Array to hold reactions ("topics")
+
+// Create a button for each "topic" (write HTML to the DOM)
+
+// Create on-click event for each button that will...
+// Call the GIPHY API and return 10 random static images using the topic as a search parameter
+
+// Create on-click event for the gifs to animate when clicked...
+// And pause when clicked again
+
+// Display the rating for each gif (DO THIS ONLY AFTER YOU HAVE GOTTEN PREVIOUS STEPS TO WORK)
+
+// Using the input value from the form, add the user's reaction to the topics array...
+// And create a new button for each new topic
+
+
